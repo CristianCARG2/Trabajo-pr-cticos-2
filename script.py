b6c7f6de0c50d22f3e3f167383421647204e2468
CristianCARG2 <cristianalberto12342@gmail.com>
2017-11-27 14:39:40 +0100

    Mi primer script

---
+++ script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+miarray = ['silvia','federico','miguel','julia','evaristo']
+print miarray[2]
+for elem in miarray:
+  print elem, " es un valor contenido en el array miarray"
+for index in range(len(miarray)):
+  print miarray[index], " es el valor contenido en el array sub ", index
+
+index=0
+for elem in miarray:
+  print index, ":", elem
+  index += 1
+miarray.append('carlos')
+
+miarray.index('carlos')
+
+miarray.index('carlitos')
+'miguel' in miarray
+'carlitos' in miarray
+
